0d981fe06ee72f71434b9f4b42acf9629dff6815
Lance Martin <martinsogunsina@gmail.com>
2025-01-07 23:20:17 +0100

    Project init and Hero conversion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="relative min-h-screen w-screen overflow-x-hidden">
+        <Hero />
+      </h1>
+    </main>
+  );
+};
+
+export default App;
