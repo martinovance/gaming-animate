c642a186ba0186a358ea811e00479241cd5db302
Lance Martin <martinsogunsina@gmail.com>
2025-01-09 21:45:58 +0100

    Navbar animate implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main>
       <h1 className="relative min-h-screen w-screen overflow-x-hidden">
+        <Navbar />
         <Hero />
         <About />
       </h1>
