5d64c4350ceea3e074fa56ab6c4a837607bd497b
Lance Martin <martinsogunsina@gmail.com>
2025-01-10 18:06:58 +0100

    UI fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import AnimatedTitle from "./AnimatedTitle";
+import Button from "./Button";
 
 const ImageClipBox = ({ src, clipClass }) => (
   <div className={clipClass}>
@@ -18,8 +20,8 @@ const Contact = () => {
         overflow-hidden sm:block lg:left-20 lg:w-96"
         >
           <ImageClipBox
-            clipClass="contact-clip-path-1"
             src="img/contact-1.webp"
+            clipClass="contact-clip-path-1"
           />
           <ImageClipBox
             clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
@@ -42,15 +44,14 @@ const Contact = () => {
         </div>
 
         <div className="flex flex-col items-center text-center">
-          <div className="font-general text-[10px] uppercase">
-            <p
-              className="special-font mt-10 w-full font-zentry text-5xl 
-            leading-[0.9] md:text-[6rem]"
-            >
-              Let&apos;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming
-              t<b>o</b>gether
-            </p>
-          </div>
+          <p className="mb-10 font-general text-[10px] uppercase">
+            Join Zentry
+          </p>
+          <AnimatedTitle
+            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
+            className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
+          />
+          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
         </div>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,11 @@ const links = [
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-violet-300 py-4 text-black">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md: flex-row">
+    <footer className="w-screen bg-violet-300 py-4 px-4 text-black">
+      <div
+        className="container mx-auto flex flex-col items-center 
+      justify-between gap-4 px-4 md:flex-row"
+      >
         <p className="text-center text-sm md:text-left">
           &copy; Nova 2024, All rights reserved
         </p>
